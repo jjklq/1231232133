2b4932cc4a718d634de1794be5996bbcb04695f5
jjklq <148258631+jjklq@users.noreply.github.com>
2023-10-22 14:30:05 +0300

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,36 +1,13 @@
-def jaccard_similarity(set1, set2):
-    if len(set1) == 0 or len(set2) == 0:
-        return 0.0
-    intersection = len(set1.intersection(set2))
-    union = len(set1.union(set2))
-    return float(intersection) / union
-
-def bus_timing(bus_stop, minutes):
-    if bust_stop in a3_bus:
-        for time in a3_bus[bus_stop]:
-            if time >= minutes:
-                return time - minutes
-    return -1
-
-    def common_courses(studen1, student2):
-        if student in student_courses and student in student_courses:
-            courses1 = student_courses[student1]
-            courses2 = student_courses[student2]
-            common_courses = list(courses.intersection(course2))
-            return common_courses
-        return[]
-
-
-from collections import deque 
+from collections import deque
 
 def shortest_route(subway_system, start, end):
     if start not in subway_system or end not in subway_system:
         return None
+    
+    visited = set()
+    queue = deque([(start, [])])
 
-visited = set()
-queue = deque([(start, [])])
-
-  while queue:
+    while queue:
         station, path = queue.popleft()
         if station == end:
             return path + [station]
@@ -38,6 +15,7 @@ queue = deque([(start, [])])
         for neighbor in subway_system[station]:
             if neighbor not in visited:
                 queue.append((neighbor, path + [station]))
+
 
 subway_system = {
     'Station A': {'Station B'},
@@ -48,4 +26,11 @@ subway_system = {
     'Station F': {'Station D', 'Station E'}
 }
 
-print(shortest_route(subway_system, 'Station A', 'Station F'))
+start_station = 'Station A'
+end_station = 'Station F'
+
+shortest_path = shortest_route(subway_system, start_station, end_station)
+if shortest_path:
+    print("Shortest Route:", shortest_path)
+else:
+    print("route not found.")
